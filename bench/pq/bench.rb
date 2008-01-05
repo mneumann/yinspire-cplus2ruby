67b000b5d395bf5adc9428cb113f5e6fa6f581ca
Michael Neumann <mneumann@ntecs.de>
2008-01-05 02:09:25 +0100

    *

---
+++ bench/pq/bench.rb
@@ -0,0 +1,114 @@
+#WARMUP_CYCLES = 100_000_000 
+#EMPTY_HOLD_CYCLES = 100_000_000 
+#HOLD_CYCLES = 100_000_000 
+
+WARMUP_CYCLES = 10_000_000 
+EMPTY_HOLD_CYCLES = 100_000_000 
+HOLD_CYCLES = 100_000_000 
+
+DISTRIBUTIONS = [
+  'Random',
+  'Exponential 1', 
+  'Uniform 0 2', 
+  'Triangular 0 1.5',
+  'NegativeTriangular 0 1.5'
+]
+
+ALGORITHMS = [
+  'BinaryHeap FLOAT',
+  'BinaryHeap DOUBLE',
+  'BinaryHeap STIMULI'
+]
+
+REPEAT = 5 
+
+QUEUE_SIZES = (1..7).map {|x| 10**x}
+#QUEUE_SIZES = [1000] #(1..6).map {|x| 10**x}
+
+def bench(queue_size, warmup_cycles, empty_hold_cycles, hold_cycles, distribution, algorithm) 
+  cmd =  "./bench #{queue_size} #{warmup_cycles} #{empty_hold_cycles} #{hold_cycles}"
+  cmd << " #{distribution} #{algorithm}"
+  parse_result(`#{cmd}`)
+end
+
+def parse_result(str)
+  h = {}
+  str.split("\n").each do |line|
+    line.strip!
+    next if line.empty?
+    key, value = line.split(":", 2)
+    h[key] = value.strip
+  end
+  h
+end
+
+def bench_algorithm(algorithm, prefix, repeat=nil, distributions=nil, queue_sizes=nil)
+  repeat ||= REPEAT
+  distributions ||= DISTRIBUTIONS
+  queue_sizes ||= QUEUE_SIZES
+
+  h = {}
+  repeat.times do 
+    distributions.each do |distr|
+      queue_sizes.each do |qs|
+        res = bench(qs, WARMUP_CYCLES, EMPTY_HOLD_CYCLES, HOLD_CYCLES, distr, algorithm)
+        h[res['Distribution']] ||= {}
+        (h[res['Distribution']][res['QueueSize']] ||= []) << res['HoldTime'].to_f
+      end
+    end
+  end
+
+  dat = File.open(prefix + ".dat", "w+") 
+  plot = File.open(prefix + ".plot", "w+")
+
+  plot << %{
+    set terminal pdf
+    set output "#{prefix}.pdf"
+    set xlabel "Queue size"
+    set ylabel "Mean access time (ns)"
+    set logscale x
+    set grid x y
+    set size ratio 1
+    set data style linespoints
+    set key top left
+    set key box
+  }
+
+  index = 0
+  h.each do |dis, h2|
+    dat.puts "# Distribution: #{dis}"
+    dat.puts "# queue size    mean access time (ns)"
+
+    if index == 0
+      plot.print "plot "
+    else
+      plot.print ",\\\n"
+    end
+
+    plot.print %{  "#{prefix}.dat" index #{index} using 1:2 title "#{dis}"}
+    index += 1
+
+    arr = []
+    h2.each do |qs, times|
+      avg = (times.inject(0.0) {|i, s| s + i}) / times.size 
+      avg *= 1_000_000_000 # convert to ns
+      arr << [qs, avg]
+    end
+    arr.sort_by {|qs,_| qs}.each do |qs, avg|
+      dat.print qs.to_s.ljust(11)
+      dat.puts avg.to_s.ljust(24)
+    end
+
+    dat.puts
+    dat.puts
+  end
+
+  plot.puts
+  dat.close
+  plot.close
+
+  system "gnuplot #{prefix}.plot"
+end 
+
+#bench_algorithm("BinaryHeap STIMULI", "work/a1")
+bench_algorithm("BinaryHeap STIMULI", "work/a2", 1, ["Random"], [100_000, 1_000_000, 10_000_000])
