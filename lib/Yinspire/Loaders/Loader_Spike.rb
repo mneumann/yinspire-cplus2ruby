983c6cf7485771188c81d2fc954cd69d26e9008b
Michael Neumann <mneumann@ntecs.de>
2008-03-31 19:34:08 +0200

    Commit 27c15f549ff70a9a5ca76d46de09b1c1263ab50c missed the inclusion of this file.

---
+++ lib/Yinspire/Loaders/Loader_Spike.rb
@@ -0,0 +1,34 @@
+require 'Yinspire/Loaders/Loader'
+
+#
+# Loader for spikes in the format:
+#
+#   Id1 weight1@time1 time2 time3 ...
+#   Id2 time1 time2 time3 ...
+#   ...
+#
+# Weight values (e.g. 1.0@time1) are optional. 
+#
+# Lines beginning with "#" are comments.
+#
+class Loader_Spike < Loader
+
+  def load(filename)
+    File.open(filename, 'r') do |f|
+      while line = f.gets
+        line.strip!
+        next if line =~ /^#/ # comment
+        id, *spikes = line.split
+        raise if spikes.empty?
+        entity = @entities[id] || raise
+
+        spikes.each do |spike|
+          weight, at = spike.split("@") 
+          weight, at = Infinity, weight if at.nil? # spike is a pure time-value
+          entity.stimulate(at.to_f, weight.to_f, nil)
+        end
+      end
+    end
+  end
+
+end
